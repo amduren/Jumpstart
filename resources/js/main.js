c0df2e6b430fc4aebfc40c165d76fffc1386bb08
Anthony Duren <amduren@gmail.com>
2018-01-07 22:09:57 -0600

    Make jumbotron work as carousel

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
